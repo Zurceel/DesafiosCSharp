95a5840884197773ea67988e354b7728245e0680
Zurceel <gabrielcruzsil@gmail.com>
2022-08-11 17:13:09 -0300

    deletando o codigo comentado

--- DesafiosCSharp/Program.cs
+++ DesafiosCSharp/Program.cs
@@ -1,24 +1,4 @@
-﻿//Console.WriteLine("Digite a sua idade");
-//int idade = Convert.ToInt32(Console.ReadLine());
-
-//if (idade <= 59)
-//{
-//	Console.WriteLine("Você ainda não tem direto a tirar o cartão idoso da prefeitura");
-//	int anosfaltantes = 60 - idade;
-//	int diasfaltantes = anosfaltantes * 365;
-//	Console.WriteLine("Faltam ainda " + anosfaltantes + "anos para tirar o cartão");
-//	Console.WriteLine("Faltam ainda " + diasfaltantes + "dias aproximadamente para tirar o cartão");
-//	idade++;
-//	int umanomaisvelho = idade++;
-//	diasfaltantes = (60 - umanomaisvelho) * 365;
-//	Console.WriteLine("Faltaria " + diasfaltantes + "dias aproximadamente para tirar o seu cartão se você tivesse um ano a mais");
-//}
-//else
-//{
-//	Console.WriteLine("Você tem direto a tirar o cartão idoso da prefeitura");
-//}
-
-string nomejogador;
+﻿string nomejogador;
 
 Console.WriteLine("Bem-vindos ao toneio de Poker!");
 
