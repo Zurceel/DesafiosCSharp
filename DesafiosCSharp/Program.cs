657479f875c72a4ef9fda1510e4352138e17126b
Zurceel <gabrielcruzsil@gmail.com>
2022-08-11 16:00:17 -0300

    testando o commit pela linha de comando

--- DesafiosCSharp/Program.cs
+++ DesafiosCSharp/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿Console.WriteLine("Digite a sua idade");
+int idade = Convert.ToInt32(Console.ReadLine());
+
+if (idade >= 60)
+{
+	Console.WriteLine("Você tem direto a tirar o cartão idoso da prefeitura");
+}else
+{
+	Console.WriteLine("Você ainda não tem direto a tirar o cartão idoso da prefeitura");
+}
